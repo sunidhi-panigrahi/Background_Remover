8d6cf9d9ed7cad335e0ea02aef5464c6968409cf
sunidhi-panigrahi <sunidhipanigrahi@gmail.com>
2025-04-19 15:35:15 +0530

    Removed Payment

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,15 +1,15 @@
 import { Webhook } from "svix"
 import userModel from "../models/userModel.js"
-import transactionModel from "../models/transactionModel.js"
-import razorpay from 'razorpay';
-import stripe from "stripe";
+// import transactionModel from "../models/transactionModel.js"
+// import razorpay from 'razorpay';
+// import stripe from "stripe";
 
 // Gateway Initialize
-const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY)
-const razorpayInstance = new razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET,
-})
+// const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY)
+// const razorpayInstance = new razorpay({
+//     key_id: process.env.RAZORPAY_KEY_ID,
+//     key_secret: process.env.RAZORPAY_KEY_SECRET,
+// })
 
 // API Controller Function to Manage Clerk User with database
 const clerkWebhooks = async (req, res) => {
@@ -91,232 +91,232 @@ const userCredits = async (req, res) => {
 
 
 // Payment API to add credits ( RazorPay )
-const paymentRazorpay = async (req, res) => {
-    try {
+// const paymentRazorpay = async (req, res) => {
+//     try {
 
-        const { clerkId, planId } = req.body
+//         const { clerkId, planId } = req.body
 
-        const userData = await userModel.findOne({ clerkId })
+//         const userData = await userModel.findOne({ clerkId })
 
-        // checking for planId and userdata
-        if (!userData || !planId) {
-            return res.json({ success: false, message: 'Invalid Credentials' })
-        }
+//         // checking for planId and userdata
+//         if (!userData || !planId) {
+//             return res.json({ success: false, message: 'Invalid Credentials' })
+//         }
 
-        let credits, plan, amount, date
+//         let credits, plan, amount, date
 
-        // Switch Cases for different plans
-        switch (planId) {
-            case 'Basic':
-                plan = 'Basic'
-                credits = 100
-                amount = 10
-                break;
+//         // Switch Cases for different plans
+//         switch (planId) {
+//             case 'Basic':
+//                 plan = 'Basic'
+//                 credits = 100
+//                 amount = 10
+//                 break;
 
-            case 'Advanced':
-                plan = 'Advanced'
-                credits = 500
-                amount = 50
-                break;
+//             case 'Advanced':
+//                 plan = 'Advanced'
+//                 credits = 500
+//                 amount = 50
+//                 break;
 
-            case 'Business':
-                plan = 'Basic'
-                credits = 5000
-                amount = 250
-                break;
+//             case 'Business':
+//                 plan = 'Basic'
+//                 credits = 5000
+//                 amount = 250
+//                 break;
 
-            default:
-                return res.json({ success: false, message: 'plan not found' })
-        }
+//             default:
+//                 return res.json({ success: false, message: 'plan not found' })
+//         }
 
-        date = Date.now()
+//         date = Date.now()
 
-        // Creating Transaction Data
-        const transactionData = {
-            clerkId,
-            plan,
-            amount,
-            credits,
-            date
-        }
+//         // Creating Transaction Data
+//         const transactionData = {
+//             clerkId,
+//             plan,
+//             amount,
+//             credits,
+//             date
+//         }
 
-        // Saving Transaction Data to Database
-        const newTransaction = await transactionModel.create(transactionData)
+//         // Saving Transaction Data to Database
+//         const newTransaction = await transactionModel.create(transactionData)
 
-        // Creating options to create razorpay Order
-        const options = {
-            amount: amount * 100,
-            currency: process.env.CURRENCY,
-            receipt: newTransaction._id,
-        }
+//         // Creating options to create razorpay Order
+//         const options = {
+//             amount: amount * 100,
+//             currency: process.env.CURRENCY,
+//             receipt: newTransaction._id,
+//         }
 
-        // Creating razorpay Order
-        await razorpayInstance.orders.create(options, (error, order) => {
-            if (error) {
-                console.log(error);
-                return res.json({ success: false, message: error });
-            }
-            res.json({ success: true, order });
-        })
+//         // Creating razorpay Order
+//         await razorpayInstance.orders.create(options, (error, order) => {
+//             if (error) {
+//                 console.log(error);
+//                 return res.json({ success: false, message: error });
+//             }
+//             res.json({ success: true, order });
+//         })
 
-    } catch (error) {
-        console.log(error)
-        res.json({ success: false, message: error.message })
-    }
-}
+//     } catch (error) {
+//         console.log(error)
+//         res.json({ success: false, message: error.message })
+//     }
+// }
 
 // API Controller function to verify razorpay payment
-const verifyRazorpay = async (req, res) => {
-    try {
+// const verifyRazorpay = async (req, res) => {
+//     try {
 
-        const { razorpay_order_id } = req.body;
+//         const { razorpay_order_id } = req.body;
 
-        // Fetching order data from razorpay
-        const orderInfo = await razorpayInstance.orders.fetch(razorpay_order_id);
+//         // Fetching order data from razorpay
+//         const orderInfo = await razorpayInstance.orders.fetch(razorpay_order_id);
 
-        // Checking for payment status
-        if (orderInfo.status === 'paid') {
-            const transactionData = await transactionModel.findById(orderInfo.receipt)
-            if (transactionData.payment) {
-                return res.json({ success: false, message: 'Payment Failed' })
-            }
+//         // Checking for payment status
+//         if (orderInfo.status === 'paid') {
+//             const transactionData = await transactionModel.findById(orderInfo.receipt)
+//             if (transactionData.payment) {
+//                 return res.json({ success: false, message: 'Payment Failed' })
+//             }
 
-            // Adding Credits in user data
-            const userData = await userModel.findOne({ clerkId: transactionData.clerkId })
-            const creditBalance = userData.creditBalance + transactionData.credits
-            await userModel.findByIdAndUpdate(userData._id, { creditBalance })
+//             // Adding Credits in user data
+//             const userData = await userModel.findOne({ clerkId: transactionData.clerkId })
+//             const creditBalance = userData.creditBalance + transactionData.credits
+//             await userModel.findByIdAndUpdate(userData._id, { creditBalance })
 
-            // Marking the payment true 
-            await transactionModel.findByIdAndUpdate(transactionData._id, { payment: true })
+//             // Marking the payment true 
+//             await transactionModel.findByIdAndUpdate(transactionData._id, { payment: true })
 
-            res.json({ success: true, message: "Credits Added" });
-        }
-        else {
-            res.json({ success: false, message: 'Payment Failed' });
-        }
+//             res.json({ success: true, message: "Credits Added" });
+//         }
+//         else {
+//             res.json({ success: false, message: 'Payment Failed' });
+//         }
 
-    } catch (error) {
-        console.log(error);
-        res.json({ success: false, message: error.message });
-    }
-}
+//     } catch (error) {
+//         console.log(error);
+//         res.json({ success: false, message: error.message });
+//     }
+// }
 
-// Payment API to add credits ( Stripe )
-const paymentStripe = async (req, res) => {
-    try {
+// // Payment API to add credits ( Stripe )
+// const paymentStripe = async (req, res) => {
+//     try {
 
-        const { clerkId, planId } = req.body
-        const { origin } = req.headers
+//         const { clerkId, planId } = req.body
+//         const { origin } = req.headers
 
-        const userData = await userModel.findOne({ clerkId })
+//         const userData = await userModel.findOne({ clerkId })
 
-        // checking for planId and userdata
-        if (!userData || !planId) {
-            return res.json({ success: false, message: 'Invalid Credentials' })
-        }
+//         // checking for planId and userdata
+//         if (!userData || !planId) {
+//             return res.json({ success: false, message: 'Invalid Credentials' })
+//         }
 
-        let credits, plan, amount, date
+//         let credits, plan, amount, date
 
-        // Switch Cases for different plans
-        switch (planId) {
-            case 'Basic':
-                plan = 'Basic'
-                credits = 100
-                amount = 10
-                break;
+//         // Switch Cases for different plans
+//         switch (planId) {
+//             case 'Basic':
+//                 plan = 'Basic'
+//                 credits = 100
+//                 amount = 10
+//                 break;
 
-            case 'Advanced':
-                plan = 'Advanced'
-                credits = 500
-                amount = 50
-                break;
+//             case 'Advanced':
+//                 plan = 'Advanced'
+//                 credits = 500
+//                 amount = 50
+//                 break;
 
-            case 'Business':
-                plan = 'Basic'
-                credits = 5000
-                amount = 250
-                break;
+//             case 'Business':
+//                 plan = 'Basic'
+//                 credits = 5000
+//                 amount = 250
+//                 break;
 
-            default:
-                return res.json({ success: false, message: 'plan not found' })
-        }
+//             default:
+//                 return res.json({ success: false, message: 'plan not found' })
+//         }
 
-        date = Date.now()
+//         date = Date.now()
 
-        // Creating Transaction Data
-        const transactionData = {
-            clerkId,
-            plan,
-            amount,
-            credits,
-            date
-        }
+//         // Creating Transaction Data
+//         const transactionData = {
+//             clerkId,
+//             plan,
+//             amount,
+//             credits,
+//             date
+//         }
 
-        // Saving Transaction Data to Database
-        const newTransaction = await transactionModel.create(transactionData)
+//         // Saving Transaction Data to Database
+//         const newTransaction = await transactionModel.create(transactionData)
 
-        const currency = process.env.CURRENCY.toLocaleLowerCase()
+//         const currency = process.env.CURRENCY.toLocaleLowerCase()
 
-        // Creating line items to for Stripe
-        const line_items = [{
-            price_data: {
-                currency,
-                product_data: {
-                    name: "Credit Purchase"
-                },
-                unit_amount: transactionData.amount * 100
-            },
-            quantity: 1
-        }]
+//         // Creating line items to for Stripe
+//         const line_items = [{
+//             price_data: {
+//                 currency,
+//                 product_data: {
+//                     name: "Credit Purchase"
+//                 },
+//                 unit_amount: transactionData.amount * 100
+//             },
+//             quantity: 1
+//         }]
 
-        const session = await stripeInstance.checkout.sessions.create({
-            success_url: `${origin}/verify?success=true&transactionId=${newTransaction._id}`,
-            cancel_url: `${origin}/verify?success=false&transactionId=${newTransaction._id}`,
-            line_items: line_items,
-            mode: 'payment',
-        })
+//         const session = await stripeInstance.checkout.sessions.create({
+//             success_url: `${origin}/verify?success=true&transactionId=${newTransaction._id}`,
+//             cancel_url: `${origin}/verify?success=false&transactionId=${newTransaction._id}`,
+//             line_items: line_items,
+//             mode: 'payment',
+//         })
         
-        res.json({ success: true, session_url: session.url });
+//         res.json({ success: true, session_url: session.url });
 
-    } catch (error) {
-        console.log(error)
-        res.json({ success: false, message: error.message })
-    }
-}
+//     } catch (error) {
+//         console.log(error)
+//         res.json({ success: false, message: error.message })
+//     }
+// }
 
-// API Controller function to verify stripe payment
-const verifyStripe = async (req, res) => {
-    try {
+// // API Controller function to verify stripe payment
+// const verifyStripe = async (req, res) => {
+//     try {
 
-        const { transactionId, success } = req.body
+//         const { transactionId, success } = req.body
 
-        // Checking for payment status
-        if (success === 'true') {
-            const transactionData = await transactionModel.findById(transactionId)
-            if (transactionData.payment) {
-                return res.json({ success: false, message: 'Payment Already Verified' })
-            }
+//         // Checking for payment status
+//         if (success === 'true') {
+//             const transactionData = await transactionModel.findById(transactionId)
+//             if (transactionData.payment) {
+//                 return res.json({ success: false, message: 'Payment Already Verified' })
+//             }
 
-            // Adding Credits in user data
-            const userData = await userModel.findOne({ clerkId: transactionData.clerkId })
-            const creditBalance = userData.creditBalance + transactionData.credits
-            await userModel.findByIdAndUpdate(userData._id, { creditBalance })
+//             // Adding Credits in user data
+//             const userData = await userModel.findOne({ clerkId: transactionData.clerkId })
+//             const creditBalance = userData.creditBalance + transactionData.credits
+//             await userModel.findByIdAndUpdate(userData._id, { creditBalance })
 
-            // Marking the payment true 
-            await transactionModel.findByIdAndUpdate(transactionData._id, { payment: true })
+//             // Marking the payment true 
+//             await transactionModel.findByIdAndUpdate(transactionData._id, { payment: true })
 
-            res.json({ success: true, message: "Credits Added" });
-        }
-        else {
-            res.json({ success: false, message: 'Payment Failed' });
-        }
+//             res.json({ success: true, message: "Credits Added" });
+//         }
+//         else {
+//             res.json({ success: false, message: 'Payment Failed' });
+//         }
 
-    } catch (error) {
-        console.log(error);
-        res.json({ success: false, message: error.message });
-    }
-}
+//     } catch (error) {
+//         console.log(error);
+//         res.json({ success: false, message: error.message });
+//     }
+// }
 
 
 
-export { clerkWebhooks, userCredits, paymentRazorpay, verifyRazorpay, paymentStripe, verifyStripe }
+export { clerkWebhooks, userCredits}
--- server/models/transactionModel.js
+++ server/models/transactionModel.js
@@ -1,14 +1,14 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const transactionSchema = new mongoose.Schema({
-    clerkId: { type: String, required: true },
-    plan: { type: String, required: true },
-    amount: { type: Number, required: true },
-    credits: { type: Number, required: true },
-    payment: { type: Boolean, default: false },
-    date: { type: Number },
-})
+// const transactionSchema = new mongoose.Schema({
+//     clerkId: { type: String, required: true },
+//     plan: { type: String, required: true },
+//     amount: { type: Number, required: true },
+//     credits: { type: Number, required: true },
+//     payment: { type: Boolean, default: false },
+//     date: { type: Number },
+// })
 
-const transactionModel = mongoose.models.transaction || mongoose.model("transaction", transactionSchema);
+// const transactionModel = mongoose.models.transaction || mongoose.model("transaction", transactionSchema);
 
-export default transactionModel;
+// export default transactionModel;
